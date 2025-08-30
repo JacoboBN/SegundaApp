194125eb77769a74597fa68d6077eaea2f48fbe1
JacoboBN <jacobobenlloch@gmail.com>
2025-08-30 18:11:28 -0500

    Añadir permisos a GitHub Actions

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Configuración de la aplicación
 APP_NAME = "SegundaApp"
-VERSION = "1.0.1"
+VERSION = "1.0.3"
 GITHUB_REPO = "JacoboBN/SegundaApp"
 UPDATE_CHECK_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 
