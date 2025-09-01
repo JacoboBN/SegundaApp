c95097702d0803531ff71d40b624affdf485895d
JacoboBN <jacobobenlloch@gmail.com>
2025-09-01 12:12:14 -0500

    version 1.0.20

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Configuración de la aplicación
 APP_NAME = "SegundaApp"
-VERSION = "1.0.19"
+VERSION = "1.0.20"
 GITHUB_REPO = "JacoboBN/SegundaApp"
 UPDATE_CHECK_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 
